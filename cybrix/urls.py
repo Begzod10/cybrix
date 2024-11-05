4c182237329abbb4845dc425e661863b5b94380a
Mannopboy <147496635+Mannopboy@users.noreply.github.com>
2024-11-05 21:49:56 +0300

    Update urls.py

--- cybrix/urls.py
+++ cybrix/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('api/', include("projects.urls")),
     path('api/', include("project_types.urls")),
     path('api/', include("languages.urls")),
-    #path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 if settings.DEBUG:
